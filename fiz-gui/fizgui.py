ed2c18215876fb9d484dd9a897c5ba6395b63b9d
brvdg <brvdg@users.noreply.github.com>
2020-10-25 13:22:26 +0100

    update for EA
    Updated files for EA Programm.

---
+++ fiz-gui/fizgui.py
@@ -0,0 +1,192 @@
+
+
+# pyuic5 -x mainwindow.ui -o mainwindow.py
+# python3.8 -m PyInstaller --onefile --add-binary='/System/Library/Frameworks/Tk.framework/Tk':'tk' --add-binary='/System/Library/Frameworks/Tcl.framework/Tcl':'tcl' fizgui.py
+
+
+from mainwindow import *
+from dialog import *
+from PyQt5 import QtCore, QtGui, QtWidgets, QtSerialPort
+
+#import serial
+#import serial.tools.list_ports
+
+#from threading import Thread
+
+import sys
+import os
+from time import sleep
+
+app = QtWidgets.QApplication(sys.argv)
+MainWindow = QtWidgets.QMainWindow()
+ui = Ui_MainWindow()
+ui.setupUi(MainWindow)
+
+Dialog = QtWidgets.QDialog()
+dia = Ui_Dialog()
+dia.setupUi(Dialog)
+
+
+serial_run = False
+configstring = False
+
+
+
+ui.pushButton.clicked.connect(lambda: connect(str(ui.comboBox.currentText())))
+ui.pushButton_send.clicked.connect(lambda: write_config())
+ui.pushButton_getConfig.clicked.connect(lambda: read_config())
+ui.lineEdit.returnPressed.connect(lambda: send_command())
+
+
+
+def connect(port):
+    global serial_run
+    ui.serial = QtSerialPort.QSerialPort(
+                port,
+                baudRate=QtSerialPort.QSerialPort.Baud9600,
+                readyRead=receive
+            )
+    if ui.serial.open(QtCore.QIODevice.ReadWrite):
+        print ("port opened")
+        serial_run = True
+
+        ui.serial.flush()
+        sleep(1)
+        ui.serial.write(str.encode("debug=0\n"))
+        ui.serial.write(str.encode("debug=1\n"))
+        ui.serial.flush()
+
+
+        read_config()
+
+
+def receive():
+    global configstring
+    #print ("read")
+
+
+    while ui.serial.canReadLine():
+        text = ui.serial.readLine().data().decode()
+        text = text.rstrip('\r\n')
+        print(text)
+        #ui.textEdit.append(text)
+
+        if text == "# fiz-o-matic configuration":
+            configstring = True
+            ui.textEdit.clear
+            #ui.textEdit.setText("# fiz-o-matic configuration")
+
+        if text == "#================#":
+            configstring = False
+
+        if ui.radioButton.isChecked():
+            ui.textEdit.append(text)
+        else:
+            if configstring:
+                if not text.startswith('['):
+                    ui.textEdit.append(text)
+
+
+
+
+def read_config():
+
+
+    ui.textEdit.clear()
+    ui.textEdit.setText("")
+
+    sleep(1)
+
+    #ser.write(str.encode('config\n'))
+    ui.serial.write(str.encode("config\n"))
+    ui.serial.flush()
+
+
+
+
+def write_config():
+    print ("write")
+    if serial_run:
+        #print(ui.textEdit.toPlainText())
+        config = ui.textEdit.toPlainText()
+
+        for line in config.splitlines():
+            #print(line)
+            line = line.rstrip()
+
+
+            if not ( line.startswith('#') or line == "" ):
+                print(line)
+                ui.serial.write(str.encode(line + "\n"))
+                ui.serial.flush()
+
+
+            #print("-")
+
+        ui.serial.write(str.encode("save\n"))
+        ui.serial.flush()
+
+        read_config()
+
+
+
+
+def send_command():
+    print (ui.lineEdit.text())
+    ui.serial.write(str.encode(ui.lineEdit.text()))
+    ui.serial.write(str.encode("\n"))
+    ui.serial.flush()
+    ui.lineEdit.setText("")
+
+
+
+def main():
+
+
+    #print (sys.platform)
+
+
+    for serialport in QtSerialPort.QSerialPortInfo.availablePorts():
+
+        if sys.platform.startswith('win'):
+            print ("found port: " + serialport.portName())
+            ui.textEdit.append("Found Port")
+            ui.textEdit.append("Port Name: " + serialport.portName())
+            ui.textEdit.append("Description: " + serialport.description())
+            ui.textEdit.append("Manufacture: " + serialport.manufacturer())
+            ui.textEdit.append("")
+
+            ui.comboBox.addItem(serialport.portName())
+
+        else:
+
+            if serialport.portName().startswith('tty'):
+                print ("found port: " + serialport.portName())
+
+                ui.textEdit.append("Found Port")
+                ui.textEdit.append("Port Name: " + serialport.portName())
+                ui.textEdit.append("Description: " + serialport.description())
+                ui.textEdit.append("Manufacture: " + serialport.manufacturer())
+                ui.textEdit.append("System Location: " + serialport.systemLocation())
+                ui.textEdit.append("")
+
+                ui.comboBox.addItem(serialport.systemLocation())
+
+                if serialport.description().startswith('Adafruit'):
+                    ui.comboBox.setCurrentText(serialport.systemLocation())
+
+
+
+    MainWindow.show()
+    sys.exit(app.exec_())
+
+
+
+
+
+
+if __name__== "__main__":
+    try:
+        main()
+    finally:
+        print ("good by")
---
+++ fiz-gui/mainwindow.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'mainwindow.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.1
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(811, 612)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.horizontalLayout_3 = QtWidgets.QHBoxLayout(self.centralwidget)
+        self.horizontalLayout_3.setObjectName("horizontalLayout_3")
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setSizeConstraint(QtWidgets.QLayout.SetMaximumSize)
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.comboBox = QtWidgets.QComboBox(self.centralwidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.comboBox.sizePolicy().hasHeightForWidth())
+        self.comboBox.setSizePolicy(sizePolicy)
+        self.comboBox.setObjectName("comboBox")
+        self.horizontalLayout.addWidget(self.comboBox)
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setEnabled(True)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pushButton.sizePolicy().hasHeightForWidth())
+        self.pushButton.setSizePolicy(sizePolicy)
+        self.pushButton.setObjectName("pushButton")
+        self.horizontalLayout.addWidget(self.pushButton)
+        spacerItem = QtWidgets.QSpacerItem(20, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem)
+        self.radioButton = QtWidgets.QRadioButton(self.centralwidget)
+        self.radioButton.setObjectName("radioButton")
+        self.horizontalLayout.addWidget(self.radioButton)
+        self.pushButton_getConfig = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_getConfig.setObjectName("pushButton_getConfig")
+        self.horizontalLayout.addWidget(self.pushButton_getConfig)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+        self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.textEdit.sizePolicy().hasHeightForWidth())
+        self.textEdit.setSizePolicy(sizePolicy)
+        self.textEdit.setSizeIncrement(QtCore.QSize(0, 0))
+        self.textEdit.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.textEdit.setObjectName("textEdit")
+        self.verticalLayout.addWidget(self.textEdit)
+        self.line = QtWidgets.QFrame(self.centralwidget)
+        self.line.setFrameShape(QtWidgets.QFrame.HLine)
+        self.line.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.line.setObjectName("line")
+        self.verticalLayout.addWidget(self.line)
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setEnabled(True)
+        self.lineEdit.setMinimumSize(QtCore.QSize(600, 0))
+        self.lineEdit.setInputMethodHints(QtCore.Qt.ImhNone)
+        self.lineEdit.setObjectName("lineEdit")
+        self.horizontalLayout_2.addWidget(self.lineEdit)
+        spacerItem1 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_2.addItem(spacerItem1)
+        self.pushButton_send = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_send.setObjectName("pushButton_send")
+        self.horizontalLayout_2.addWidget(self.pushButton_send)
+        self.verticalLayout.addLayout(self.horizontalLayout_2)
+        self.horizontalLayout_3.addLayout(self.verticalLayout)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 811, 22))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "fiz-o-matic configurator"))
+        self.pushButton.setText(_translate("MainWindow", "Connect"))
+        self.radioButton.setText(_translate("MainWindow", "Konsolen Modus"))
+        self.pushButton_getConfig.setText(_translate("MainWindow", "Get Config"))
+        self.pushButton_send.setText(_translate("MainWindow", "Send"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
